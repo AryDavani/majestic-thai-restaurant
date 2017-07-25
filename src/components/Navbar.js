50177ef0f54824d75258e3340ea04ca606e96d9d
Ary Davani <arydavani@gmail.com>
2017-07-25 18:54:20 -0400

    added Baselayout component and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,15 @@
 import React, { Component } from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <ul className="center-nav">
+          <li><a>Home</a></li>
+          <li><a>Menu</a></li>
+          <li><a>Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
