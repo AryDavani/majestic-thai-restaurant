928433f4ce8c2d0cb6aad4cfd983edae289bee11
Ary Davani <arydavani@gmail.com>
2017-07-29 10:52:16 -0400

    adding routes to url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import BaseLayout from './components/BaseLayout';
+import Menu from './components/Menu';
+import Order from './components/Order';
+import Homepage from './components/Homepage';
+import About from './components/About';
+import Contact from './components/Contact';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route path='/menu' component={Menu}/>
+        <Route path='/order' component={Order}/>
+        <Route path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/>
+        <Route path='/' component={Homepage}/>
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>
+
+  , document.getElementById('root'));
 registerServiceWorker();
