39f0d8329b37fb1b55d83f8177537e972cd8cdce
Ary Davani <arydavani@gmail.com>
2017-07-31 09:21:10 -0400

    added handle order method

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import MenuItem from './MenuItem';
 
 export default class Menu extends Component {
@@ -19,16 +19,39 @@ export default class Menu extends Component {
       category: 'Appetizers'
     }
 
+    // this._handleYourOrder = this._handleYourOrder.bind(this);
     this._handleSelect = this._handleSelect.bind(this);
     this._handleAddToOrder = this._handleAddToOrder.bind(this);
 
   }
+
+  // _handleYourOrder(event) {
+  //   event.preventDefault();
+  //
+  // }
 
   _handleSelect(event) {
     this.setState({
       category: event.target.value
     });
   }
+
+  _handleAddToOrder(menuItem) {
+    let price = (Number(menuItem.price) * menuItem.qty);
+    let total = this.state.order.total;
+    let items = this.state.order.items;
+
+    items.push(menuItem);
+    total += price;
+
+    this.setState({
+      order: {
+        total,
+        items
+      }
+    })
+  }
+
 
   componentDidMount(){
     fetch('http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu')
@@ -38,25 +61,6 @@ export default class Menu extends Component {
       this.setState({
         menu: data[0]
       });
-    })
-  }
-
-  _handleAddToOrder(menuItem) {
-    let price = (Number(menuItem.price) * menuItem.qty);
-    let total = this.state.order.total;
-    let items = this.state.order.items;
-
-    // console.log("menuItem", menuItem);
-    // console.log("totalprice", price);
-    // total = total.toFixed(2) + price
-    items.push(menuItem);
-    total += price;
-
-    this.setState({
-      order: {
-        total,
-        items
-      }
     })
   }
 
@@ -74,7 +78,7 @@ export default class Menu extends Component {
           <input value="Appetizers" type="button" onClick={ this._handleSelect } />
           <input value="Entrees" type="button" onClick={ this._handleSelect } />
           <input value="Desserts" type="button" onClick={ this._handleSelect } />
-          <NavLink to="/order"><button className="order-btn" onClick={ this.state._handleYourOrder }>Place Order {this.state.order.items.length}</button></NavLink>
+          <Link to="/order"><button className="order-btn" onClick={ this._handleYourOrder }>Place Order {this.state.order.items.length}</button></Link>
         </div>
         <div className="menu-display">
           { categoryItems }
